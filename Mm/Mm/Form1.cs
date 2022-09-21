013fd573f291a0e29ebb3659d53291668ed1f97b
FatimaNajera <94653499+FatimaNajera@users.noreply.github.com>
2022-09-21 16:19:05 -0500

    Add files via upload

---
+++ Mm/Mm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Mm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Registros = 0;
+            Conexion.EjecutaConsulta(textBox1.Text);
+            AccionesComunes.Mensaje("" + Registros);
+            AccionesComunes.LlenarCombo(textBox1.Text, comboBox1, "UserID", "Email");
+            AccionesComunes.LlenarCombo(textBox1.Text, comboBox2, "UserID", "Email");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
